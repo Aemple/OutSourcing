4262a3a495cb0c55ec23381aea6360ed7bcd7ca7
Aemple <Aemple666@163.com>
2018-12-10 18:08:18 +0800

    xx

---
+++ build/precache-manifest.2632d604e606073d5667d0726f46e455.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "95d27c9d18e63ecf22b8063f87d03102",
+    "url": "/static/media/logo.95d27c9d.png"
+  },
+  {
+    "revision": "b6b8990ab8b35605fa884257c693ccf3",
+    "url": "/static/media/js5.b6b8990a.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3f08a72c08474ebab1b3",
+    "url": "/static/js/main.3f08a72c.chunk.js"
+  },
+  {
+    "revision": "e03563c3c6f0d2a630bd",
+    "url": "/static/js/1.e03563c3.chunk.js"
+  },
+  {
+    "revision": "3f08a72c08474ebab1b3",
+    "url": "/static/css/main.5b9e8170.chunk.css"
+  },
+  {
+    "revision": "e03563c3c6f0d2a630bd",
+    "url": "/static/css/1.c77d2313.chunk.css"
+  },
+  {
+    "revision": "53dbf3586ba160e330c38d19a4f5a4ea",
+    "url": "/index.html"
+  }
+];
